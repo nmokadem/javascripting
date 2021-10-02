91c5f1279bb99db2419007fe77a85028f4239a2d
Nasser Mokadem <nmokadem@hotmail.com>
2021-10-02 00:25:11 +0000

    Initial commit

---
+++ accessing-array-values.js
@@ -0,0 +1,8 @@
+const food = ['apple', 'pizza', 'pear'];
+console.log(food[1]);
+
+// In that file, define array food :  
+   
+// const food = ['apple', 'pizza', 'pear']  
+
+// Use console.log() to print the second value of array to the terminal.
---
+++ functions.js
@@ -0,0 +1,14 @@
+const eat = function(food){
+  return food + ' tasted really good.'
+}
+console.log(eat('bananas'));
+
+// In that file, define a function named eat that takes an argument named  
+// food that is expected to be a string.  
+ 
+// Inside the function return the food argument like this:  
+ 
+//    return food + ' tasted really good.'  
+ 
+// Inside of the parentheses of console.log(), call the eat() function with  
+// the string bananas as the argument.
